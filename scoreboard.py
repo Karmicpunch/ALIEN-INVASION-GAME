d3a9a98af8fda08aa2ebe0d1460744adaa253482
KarmicPunch <koshelevsergey86@gmail.com>
2021-05-13 20:07:54 -0400

    new

--- scoreboard.py
+++ scoreboard.py
@@ -12,14 +12,16 @@ class Scoreboard:
         self.stats = ai_game.stats
 
         #  Text settings
-        self.text_color = (30, 30, 30)
+        self.text_color = (255, 99, 71)
         self.font = pygame.font.SysFont(None, 48)
-
+        # Prep score images
         self.prep_score()
+        self.prep_high_score()
 
     def prep_score(self):
         """ Formatting game score into text"""
-        score_str = str(self.stats.score)
+        rounded_score = round(self.stats.score, -1)
+        score_str = "{:,}".format(rounded_score)
         self.score_image = self.font.render(score_str, True,
                                             self.text_color, self.settings.bg_color)
 
@@ -28,6 +30,25 @@ class Scoreboard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_high_score(self):
+        """ Show record on the screen """
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = "{:,}".format(high_score)
+        self.high_score_image = self.font.render(high_score_str, True,
+                                     self.text_color, self.settings.bg_color)
+
+        # Record appears on the upper centre of the screen
+        self.high_score_rect =self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
     def show_score(self):
         """ Shows score on a screen """
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+
+    def check_high_score(self):
+        """ Checking new record """
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
